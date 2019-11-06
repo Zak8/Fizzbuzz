10923212245bbb170626e711362e8d32e1c37d0d
Zak8 <zak00rashid@gmail.com>
2019-11-06 11:29:33 +0000

    Fizzbuzz files updates

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,27 @@
+# def fizzbuzz(number)
+#   counter = 0
+#   while counter < number
+#     counter += 1
+#     if counter % 3 === 0 and counter % 5 === 0
+#       puts "FizzBuzz"
+#     elsif counter % 3 === 0
+#       puts "Fizz"
+#     elsif counter % 5 === 0
+#       puts "Buzz"
+#     else
+#       puts counter
+#     end
+#   end
+# end
+# fizzbuzz(15)
 def fizzbuzz(number)
-    'fizz'
+  if number % 3 === 0 and number % 5 === 0
+    return "fizzbuzz"
+  elsif number % 3 === 0
+    return "fizz"
+  elsif number % 5 === 0
+    return "buzz"
+  else
+    return number
+  end
 end
